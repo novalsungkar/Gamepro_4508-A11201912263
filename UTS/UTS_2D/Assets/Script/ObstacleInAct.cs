dd39866d62fa243ff135b2fc89269182503808f7
novalsungkar <85091731+novalsungkar@users.noreply.github.com>
2021-11-15 09:40:38 -0600

    Noval Sungkar
    Uts A11.2019.12263

---
+++ UTS/UTS_2D/Assets/Script/ObstacleInAct.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleInAct : MonoBehaviour
+{
+    Vector2 veloc = new Vector2(-4, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = veloc;
+        float range = 2;
+        transform.position = new Vector3(transform.position.x, transform.position.y - range * Random.value, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
